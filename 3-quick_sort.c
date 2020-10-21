f8786df4e4b3ce6ea1aa80ca36d43998052d7a6a
BigObasheer <838@holbertonschool.com>
2020-10-21 04:44:58 +0000

    Add Task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,38 +7,38 @@
  */
 void quick_sort(int *array, size_t size)
 {
-        int tmp;
-        size_t i = -1, j;
+	int tmp;
+	size_t i = -1, j;
 
-        if (size < 2)
-                return;
+	if (size < 2)
+		return;
 
-        for (j = 0; j < size - 1; j++)
-        {
-                if (array[j] <= array[size - 1])
-                {
-                        i++;
-                        if (i != j)
-                        {
-                                tmp = array[i];
-                                array[i] = array[j];
-                                array[j] = tmp;
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] <= array[size - 1])
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
 
-                                print_array(array, size);
-                        }
-                }
-        }
+				print_array(array, size);
+			}
+		}
+	}
 
-        if (i + 1 != size - 1)
-        {
-                tmp = array[i + 1];
-                array[i + 1] = array[size - 1];
-                array[size - 1] = tmp;
+	if (i + 1 != size - 1)
+	{
+		tmp = array[i + 1];
+		array[i + 1] = array[size - 1];
+		array[size - 1] = tmp;
 
-                print_array(array, size);
-        }
+		print_array(array, size);
+	}
 
 
-        quick_sort(array, i + 1);
-        quick_sort(array + (i + 1), size - i - 1);
+	quick_sort(array, i + 1);
+	quick_sort(array + (i + 1), size - i - 1);
 }
