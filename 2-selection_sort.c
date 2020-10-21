28a26b6c3965d92c098cb2faf99e642914b54de3
BigObasheer <838@holbertonschool.com>
2020-10-21 04:42:24 +0000

    Add Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,24 +7,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
-        unsigned int i, j, swap, min;
+	unsigned int i, j, swap, min;
 
-        if (!array)
-                return;
+	if (!array)
+		return;
 
-        for (i = 0; i < size - 1; i++)
-        {
-                min = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
 
-                for (j = i + 1; j < size; j++)
+		for (j = i + 1; j < size; j++)
                         if (array[j] < array[min])
-                                min = j;
-                if (min != i)
-                {
-                        swap = array[min];
-                        array[min] = array[i];
-                        array[i] = swap;
-                        print_array(array, size);
-                }
-        }
+				min = j;
+		if (min != i)
+		{
+			swap = array[min];
+			array[min] = array[i];
+			array[i] = swap;
+			print_array(array, size);
+		}
+	}
 }
