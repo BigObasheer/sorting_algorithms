21fb0f1c41a724cb1fb0031227f10a485828c33b
BigObasheer <838@holbertonschool.com>
2020-10-21 04:28:45 +0000

    Add Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function to use selection sort algorithm on array
+ * @array: array to be sorted
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+        unsigned int i, j, swap, min;
+
+        if (!array)
+                return;
+
+        for (i = 0; i < size - 1; i++)
+        {
+                min = i;
+
+                for (j = i + 1; j < size; j++)
+                        if (array[j] < array[min])
+                                min = j;
+                if (min != i)
+                {
+                        swap = array[min];
+                        array[min] = array[i];
+                        array[i] = swap;
+                        print_array(array, size);
+                }
+        }
+}
